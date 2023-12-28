8935cb5e488c4dbd379a8e0d064b7352a4c4878b
Omerfarukyy <omerfarukyy@hotmail.com>
2023-12-28 22:51:21 +0300

    With using List Comprehension and Pandas made a spelling project

--- Day26/NATO-alphabet-start/main.py
+++ Day26/NATO-alphabet-start/main.py
@@ -3,6 +3,7 @@ import pandas
 word = input()
 phonetic_alphabet_list = pandas.read_csv("nato_phonetic_alphabet.csv")
 alphabet_list = {}
+
 for (index, row) in phonetic_alphabet_list.iterrows():
     new_dict = {row.letter: row.code for item in row}
     alphabet_list.update(new_dict)
