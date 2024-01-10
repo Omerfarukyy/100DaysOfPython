896d1261fb3a97088e64e63d2c13c006bb09d5cb
Omerfarukyy <omerfarukyy@hotmail.com>
2024-01-10 13:27:42 +0300

    Checking the Weather And Send SMS With Openweather API and Twilio API

---
+++ Day35/main.py
@@ -0,0 +1,41 @@
+import requests
+from twilio.rest import Client
+
+
+account_sid = 'AC34250a225d403d0ccba6e056114a4601'
+auth_token = 'ff8caae6f749a7273549cc7963cc9bf7'
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+  from_='+15186621005',
+  body='Bring an umbrella, its a rainy day',
+  #   for the privacy, I do not show my number
+  to='+90************'
+)
+
+api_key = "f15c0866b5b9353065af54bf4a24f9b7"
+MY_LAT = 41.008240
+MY_LONG = 28.978359
+COUNT = 5
+UNIT = "metric"
+parameters = {
+    "lat": MY_LAT,
+    "lon": MY_LONG,
+    "appid": api_key,
+    "units": UNIT,
+    "cnt": COUNT,
+}
+
+
+response = requests.get(url="https://api.openweathermap.org/data/2.5/forecast", params=parameters)
+response.raise_for_status()
+data = response.json()
+weather = [w["weather"][0] for w in data["list"]]
+weather_ids = [weather[w]["id"] for w in range(len(weather))]
+weather_desc = [weather[w]["description"] for w in range(len(weather))]
+for id in weather_ids:
+    if id < 600:
+        client = Client(account_sid, auth_token)
+        break
+
+print(message.status)
