c8a5ed07f2d07122f287aa87eaef59910ae66e4e
Omerfarukyy <omerfarukyy@hotmail.com>
2024-02-22 12:26:32 +0300

    Internet Speed Complaint Bot(on Twitter/X)

--- Day51/main.py
+++ Day51/main.py
@@ -2,9 +2,7 @@ from selenium import webdriver
 from InternetSpeedTwitterBot import InternetSpeedTwitterBot
 twitter_email = "pythontestomer@gmail.com"
 twitter_password = "omerfaruk"
-
 bot = InternetSpeedTwitterBot()
 
 bot.get_internet_speed()
-
 bot.tweet_at_provider()
